691c166aa8664a69772ea23732e26f869616132b
Richard Meng <lemonbuttercookie@gmail.com>
2022-08-23 18:47:43 -0700

    Add tsParticles to Home

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useCallback } from "react";
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+import particlesOptions from "./particles.json";
 import "./style.css";
 
 function Home(props) {
+  const particlesInit = useCallback((engine) => {
+    console.log(engine);
+    loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback((container) => {
+    console.log(container);
+  }, []);
+
   return (
     <div class="home">
+      <Particles
+        options={particlesOptions}
+        init={particlesInit}
+        loaded={particlesLoaded}
+      />
       <div class="home-box">
         <h1>{props.name}</h1>
       </div>
