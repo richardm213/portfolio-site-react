5ffb0644cd19aae7ecc6b819efb42d412fc0dea7
Richard Meng <lemonbuttercookie@gmail.com>
2022-08-26 16:23:25 -0700

    Implement dark mode

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,31 @@
-import React, { useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Particles from "react-particles";
 import { loadFull } from "tsparticles";
 import particlesOptions from "./particles.json";
 import "./style.css";
 
+const updateMode = (darkMode) => {
+  const root = document.documentElement;
+  if (!darkMode) {
+    root.style.setProperty("--bg-primary", "#FFFFFF");
+    root.style.setProperty("--text-primary", "#000000");
+    root.style.setProperty("--invert", "0%");
+  } else {
+    root.style.setProperty("--bg-primary", "#000000");
+    root.style.setProperty("--text-primary", "#FFFFFF");
+    root.style.setProperty("--invert", "100%");
+  }
+};
+
 function Home(props) {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    updateMode(darkMode);
+  }, [darkMode]);
+
+  const handleNameClick = () => setDarkMode((prevMode) => !prevMode);
+
   const particlesInit = useCallback((engine) => {
     console.log(engine);
     loadFull(engine);
@@ -23,7 +44,9 @@ function Home(props) {
         loaded={particlesLoaded}
       />
       <div class="home-box">
-        <h1>{props.name}</h1>
+        <h1 id="name-header" onClick={() => handleNameClick()}>
+          {props.name}
+        </h1>
         <a href="#about-me">
           <img
             id="go-down"
